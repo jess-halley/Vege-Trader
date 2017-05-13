b46c8a1548650fa0602078b556f1b7bb0369b309
Jess-Halley <jmmhalley@gmail.com>
2017-05-13 13:17:06 +1200

    message

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL || {},
+    connection: "postgres://dpxocmmlxkznrp:f7ad3d9db052ff4eff131e7bf19a9c7b2f1187cee2c830f4bac91161eeb30e8d@ec2-50-19-83-146.compute-1.amazonaws.com:5432/d2c1clh63ige99",
     pool: {
       min: 2,
       max: 10
