745156900963826b0e498f3ba651b3122cade3c8
Jess-Halley <jmmhalley@gmail.com>
2017-05-13 12:23:35 +1200

    add postgres

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL || {}
+    connection: process.env.DATABASE_URL || {},
     pool: {
       min: 2,
       max: 10
