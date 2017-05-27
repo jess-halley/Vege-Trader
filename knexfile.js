5a88eafd2d95bcf40f7f5c0952fd9eda976715d5
Jess-Halley <jmmhalley@gmail.com>
2017-05-27 12:22:24 +1200

    updating db connection string

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: "postgres://dpxocmmlxkznrp:f7ad3d9db052ff4eff131e7bf19a9c7b2f1187cee2c830f4bac91161eeb30e8d@ec2-50-19-83-146.compute-1.amazonaws.com:5432/d2c1clh63ige99",
+    connection: "postgres://napraewlbvpqlt:08a16a76d514af1ba8db85f23c2f54e6f58a6c9bd3351146b659b36ad36472d2@ec2-184-73-236-170.compute-1.amazonaws.com:5432/d1tak7uur2okmj",
     pool: {
       min: 2,
       max: 10
--- migrations/20170525095834_vegetables.js
+++ migrations/20170525095834_vegetables.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('vegetables', function (table) {
     table.increments('id').primary()
-    table.int('user_id')
+    table.string('user_id')
     table.string('vegetables')
     table.string('quantity')
     table.string('imgURL')
